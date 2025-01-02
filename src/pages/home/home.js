fbaf3fe2245ffc9e40cce4707116d5cd9f7eba28
GuillerFuentec <copelloguillermo60@gmail.com>
2025-01-02 16:06:06 -0500

    Home now is already and finished updaded

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 import { Container } from "semantic-ui-react";
 import { BasicLayout } from "@/layouts";
-import { Separator, BarTrust } from "@/components/Shared";
+import { Separator, BarTrust, BannerAd } from "@/components/Shared";
 import { Home } from "@/components/Home";
 
 const platformID = {
@@ -15,6 +15,7 @@ export default function HomePage() {
     <>
       {/* SEO */}
       <BasicLayout>
+        {/* HERO Section */}
         <Home.BannerLastGamePublished></Home.BannerLastGamePublished>
         <Separator height={100} />
         <Container>
@@ -24,9 +25,32 @@ export default function HomePage() {
         <BarTrust />
         <Separator height={100} />
         <Container>
-          <Home.LastestGames platformID={platformID.nintendo} limit={3} />
-        <Separator height={50} />
+        {/* Plasytation Section */}
+          <Home.LastestGames
+            title="PLaystation"
+            platformID={platformID.playstation}
+            limit={3}
+          />
         </Container>
+        <Separator height={100} />
+        {/* Publicitation */}
+        <BannerAd
+          title="Sign up and get better offers"
+          subtitle="Buy with other games and choose you own"
+          btnTitle="Sign up now"
+          btnLink="/account"
+          img="/img/img01.png"
+        />
+        <Separator height={100} />
+        {/* X-BOX Games */}
+        <Container>
+          <Home.LastestGames
+            title="X-Box"
+            platformID={platformID.xbox}
+            limit={3}
+          />
+        </Container>
+        <Separator height={100} />
       </BasicLayout>
     </>
   );
